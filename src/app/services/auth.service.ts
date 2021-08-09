6a34214109a6d4da329257c573b6723010795dda
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2021-08-09 17:12:51 +0300

    add logOut

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,14 +4,42 @@ import {
   AngularFirestore,
   AngularFirestoreDocument,
 } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {}
+  private isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  private userDetails$: Subject<User> = new Subject();
+
+  constructor(
+    private afs: AngularFirestore,
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {
+    const saveUserString = localStorage.getItem('user');
+    if (saveUserString != null) {
+      this.isLoggedIn$.next(true);
+    }
+
+    afAuth.authState.subscribe((user) => {
+      if (!!user) {
+        this.userDetails$.next(<User>user);
+        const userString: string = JSON.stringify(user);
+        localStorage.setItem('user', userString);
+        this.isLoggedIn$.next(true);
+      } else {
+        localStorage.removeItem('user');
+        this.isLoggedIn$.next(false);
+      }
+    });
+  }
 
   public signInWithGoogle() {
     this.authLogin(new firebase.default.auth.GoogleAuthProvider());
@@ -36,5 +64,16 @@ export class AuthService {
     return userRef.set(userData, {
       merge: true,
     });
+  }
+
+  public signOut(): Promise<void> {
+    return this.afAuth.signOut().then(() => {
+      this.router.navigate(['/']);
+      this.userDetails$.next(undefined);
+    });
+  }
+
+  public isLoggedIn(): Observable<boolean> {
+    return this.isLoggedIn$.asObservable();
   }
 }
